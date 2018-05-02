61bceba580760ae8c3a5d38bd803ac5cd8f9aff2
Mark Kelley <mark.kelley@avigilon.com>
2018-05-02 15:45:20 -0400

    Add API client

--- TVSchedule/API/TVMazeClient.swift
+++ TVSchedule/API/TVMazeClient.swift
@@ -7,3 +7,51 @@
 //
 
 import Foundation
+
+enum ApiResult<T : Decodable> {
+    case success(T)
+    case failure(ApiError)
+}
+
+enum ApiError : Error {
+    case connectionError(Error)
+    case serverError(Int)
+    case responseFormatInvalid(String)
+}
+
+class TVMazeClient {
+    let session: URLSession
+    
+    init(session: URLSession) {
+        self.session = session
+    }
+    //2014-05-02
+    func fetchShowList(for date: String, completion: @escaping (ApiResult<[Show]>) -> Void) {
+        let url = URL(string: "http://api.tvmaze.com/schedule?country=US&date=\(date)")!
+        let req = URLRequest(url: url)
+        
+        let task = session.dataTask(with: req) { (data, response, error) in
+            if let err = error {
+                completion(.failure(.connectionError(err)))
+            } else {
+                let httpResponse = response as! HTTPURLResponse
+                
+                switch httpResponse.statusCode {
+                case 200:
+                    let decoder = JSONDecoder()
+                    
+                    do {
+                        let shows = try decoder.decode([Show].self, from: data!)
+                        completion(.success(shows))
+                    } catch let e {
+                        completion(.failure(.responseFormatInvalid(e.localizedDescription)))
+                    }
+                default:
+                    completion(.failure(.serverError(httpResponse.statusCode)))
+                }
+            }
+        }
+        
+        task.resume()
+    }
+}
